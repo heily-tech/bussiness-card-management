4734d4946b4ff8ee074706c3cb665e3f484674a4
heily-tech <edelbteen@gmail.com>
2022-06-02 17:01:01 +0900

    modify DAO

--- WEB_INF/src/action/CardListAction.java
+++ WEB_INF/src/action/CardListAction.java
@@ -12,7 +12,7 @@ public class CardListAction implements Action {
 
 	 public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception{
 
-		ArrayList<CardBean> articleList=new ArrayList<CardBean>(); //전체 명함 목록 저장.
+		ArrayList<CardBean> cardList=new ArrayList<CardBean>(); //전체 명함 목록 저장.
 	  	int page=1;
 		  int limit=10;
 
@@ -22,7 +22,7 @@ public class CardListAction implements Action {
 
 		CardListService cardListService = new CardListService();
 		int listCount=CardListService.getListCount(); //총 명함의 수를 받아옴.
-		articleList = CardListService.getCardList(page,limit); //페이지에 출력될 명함 목.
+		cardList = CardListService.getCardList(page,limit); //페이지에 출력될 명함 목.
 		int maxPage=(int)((double)listCount/limit+0.95); //총 페이지 수.0.95를 더해서 올림 처리.
 		int startPage = (((int) ((double)page / 10 + 0.9)) - 1) * 10 + 1; //현재 페이지에 보여줄 시작 페이지 수.(1, 11, 21...)
 		int endPage = startPage+10-1;	//현재 페이지에 보여줄 마지막 페이지 수.(10, 20, 30...)
--- WEB_INF/src/dao/CardDAO.java
+++ WEB_INF/src/dao/CardDAO.java
@@ -17,8 +17,6 @@ public class CardDAO {
 
 	private CardDAO() {}
 
-
-	//싱글톤 패턴으로 cardDAO 객체가 null이면 새로운 객체를 생성 후 반환
 	public static CardDAO getInstance() {
 		if(cardDAO == null) {
 			cardDAO = new CardDAO();
@@ -26,38 +24,25 @@ public class CardDAO {
 		return cardDAO;
 	}
 
-	//접근자는 여기서 가지고 오지 않음
 	public void setCon(Connection con) {
 		this.con = con;
 	}
 
-	//글 목록보기
-	//vo 객체 등 여러 객체와 클래스가 없는 관계로 테스트 불가능.
+	/***** 명함 목록보기 *****/
 	public ArrayList<CardBean> selectCardList(/* 파라미터*/){
-		//pstmt과 rs 초기화
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		//직급과 사원번호 순으로 오름차순 정렬
 		String card_list_sql="select * from bcard order by pos_num, emp_num asc";
-		//vo 객체에 담을 arraylist 초기화
 		ArrayList<CardBean> cardList = new ArrayList<CardBean>();
-		//vo 객체 초기화
-		CardBean card = new CardBean;
-		/*vo객체 선언 필요*/
-		card = null;
+		CardBean card = null;
+		int startrow = (page -1) * 10;
 
-
-		//오류처리 및 설정자를 이용해서, 접근자로 불러온 데이터를 저장.
-
-		//vo객체로 인해 직렬화 불필요.
 		try {
-			//정렬된 목록 불러오기
 			pstmt = con.prepareStatement(board_list_sql);
+			pstmt.setInt(1, startrow);
 			rs = pstmt.executeQuery();
 
-			//card 객체에 더이상 검색되는 데이터가 없을 때까지 넣음.
 			while(rs.next()) {
-				//새로운 vo객체 생성
 				card = new CardBean;
 				card.setEMP_NUM(rs.getInt("EMP_NUM"));
 				card.setNAME_KOR(rs.getString("NAME_KOR"));
@@ -72,14 +57,51 @@ public class CardDAO {
 				card.setD_RESIGN(rs.getDate("D_RESIGN"));
 				cardList.add(card);
 			}
-		}catch(Exception e) {
+		} catch(Exception e) {
 			System.out.println("getCardList 에러 : " + e);
-		}finally {
+		} finally {
 			close(rs);
 			close(pstmt);
-		}
-	return cardList;
-		
+		} return cardList;
+	}
+
+	/***** 명함 삽입 *****/
+	public int insertCardList(CardBean list) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "";
+		int insertCount = 0;
+
+		//패스워드 포함 13개의 db 콜론 전부 입력(초기 입력일 경우, 패스워드와 퇴사일 입력은 고려해볼 내용!!)
+		try {
+			sql = "insert into bcard"
+					+ "(emp_num," + "emp_passwd," + "soc_num," + "name_kor," + "name_eng," + "dep_num," + "pos_num,"
+					+ "mobile," + "phone," + "fax,"	+ "email," + "d_entry," + "d_resign"
+					+ " values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, list.getEMP_NUM());
+			pstmt.setString(2, list.getEMP_PASSWD());
+			pstmt.setInt(3, list.getSOC_NUM());
+			pstmt.setString(4, list.getNAME_KOR());
+			pstmt.setString(5, list.getNAME_ENG());
+			pstmt.setInt(6, list.getDEP_NUM());
+			pstmt.setInt(7, list.getPOS_NUM());
+			pstmt.setInt(8, list.getMOBILE());
+			pstmt.setInt(9, list.getPHONE());
+			pstmt.setInt(10, list.getFAX());
+			pstmt.setString(11, list.getEMAIL());
+			pstmt.setDate(12, list.getD_ENTRY());
+			pstmt.setDate(13, list.getD_RESIGN());
+
+			//insert 실행하고 성공한 횟수를 insertCount에 반환
+			insertCount = pstmt.executeUpdate();
+		} catch(Exception e) {
+			System.out.println("insertCardList 에러 : e");
+		} finally {
+			close(rs);
+			close(pstmt);
+		} return insertCount;
 	}
 	
 }

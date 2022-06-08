fdc2a150a2c2e9b80774e56ad5d73736117f6988
dls0 <91050566+dls0@users.noreply.github.com>
2022-06-08 10:39:32 +0900

    Update CardDetailAction.java

--- WEB_INF/src/action/CardDetailAction.java
+++ WEB_INF/src/action/CardDetailAction.java
@@ -8,14 +8,14 @@ import vo.CardBean;
 
 public class CardDetailAction implements Action {
 	public ActionForward execute (HttpServletRequest request, HttpServletResponse response) throws Exception {
-		int emp_num = Integer.parseInt(request.getParameter("emp_num"));
-		String page = request.getParameter("page");
+		int emp_num = Integer.parseInt(request.getParameter("emp_num")); //상세 내용플 보고자하는 사원 번호
+		String page = request.getParameter("page"); //파라미터로 전송되는 페이지 번호를 받음
 		CardDetailService cardDetailService = new CardDetailService();
-		CardBean article = cardDetailService.selectCard(emp_num);
+		CardBean article = cardDetailService.selectCard(emp_num); //파라미터로 전송된 사원 번호의 사원 정보를 반환
 		ActionForward forward = new ActionForward();
 		request.setAttribute("page", page);
 		request.setAttribute("article", article);
-		forward.setPath("/webapp/card_detail.jsp");
+		forward.setPath("/webapp/card_detail.jsp"); //받아온 정보를 상세보기 페이지로 넘김
 		return forward;
 	}
 }
